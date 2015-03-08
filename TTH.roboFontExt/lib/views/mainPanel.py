a2e98741b2a86562b603cc984d889c0123ed9611
Jeremie Hornus <jeremiehornus@iMac-de-Jeremie.local>
2015-03-08 21:26:38 +0100

    views added

---
+++ TTH.roboFontExt/lib/views/mainPanel.py
@@ -0,0 +1,5 @@
+from defconAppKit.windows.baseWindow import BaseWindowController
+
+class mainPanel(BaseWindowController):
+	def __init__(self, TTHToolInstance):
+		BaseWindowController.__init__(self)
