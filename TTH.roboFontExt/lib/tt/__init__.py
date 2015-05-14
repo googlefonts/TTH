49c75eaf7df66007054befec835b33fd242c5e47
Samuel Hornus <samuel.hornus@inria.fr>
2015-05-14 23:44:50 +0200

    easier reading of code

--- TTH.roboFontExt/lib/tt/__init__.py
+++ TTH.roboFontExt/lib/tt/__init__.py
@@ -2,30 +2,31 @@
 from commons import helperFunctions as HF
 
 def compare(A, B):
-	#A_isSingleLink  = A['code'] in ['singleh', 'singlev']
-	#B_isSingleLink  = B['code'] in ['singleh', 'singlev']
-	#A_isInterpolate = A['code'] in ['interpolateh', 'interpolatev']
-	#B_isInterpolate = B['code'] in ['interpolateh', 'interpolatev']
 	A_isMiddleDelta = A['code'] in ['mdeltah', 'mdeltav']
 	B_isMiddleDelta = B['code'] in ['mdeltah', 'mdeltav']
 	A_isFinalDelta  = A['code'] in ['fdeltah', 'fdeltav']
 	B_isFinalDelta  = B['code'] in ['fdeltah', 'fdeltav']
+	ba = (True, True)
+	ab = (True, False)
+	dontcare = (False, False)
 
 	if ((A_isMiddleDelta and B_isMiddleDelta) and (A['point'] == B['point'])) \
 	or (A_isFinalDelta and B_isFinalDelta):
+		# Two middle deltas on the same point
+		# OR two final deltas: we group deltas with same type together
 		if A['mono'] == 'true': Avalue = 2
 		else: Avalue = 0
 		if A['gray'] == 'true': Avalue += 1
 		if B['mono'] == 'true': Bvalue = 2
 		else: Bvalue = 0
 		if B['gray'] == 'true': Bvalue += 1
-		if Avalue < Bvalue:   return (True, True) # order == B->A
-		elif Avalue > Bvalue: return (True, False) # order == A->B
-		else: return (False, False)
-	elif A_isFinalDelta:
-		return (True, True) # order == B->A
-	elif B_isFinalDelta:
-		return (True, False) # order == A->B
+		if Avalue < Bvalue:   return ba
+		elif Avalue > Bvalue: return ab
+		else: return dontcare
+	elif A_isFinalDelta: # B is NOT final delta, so B goes before A
+		return ba
+	elif B_isFinalDelta: # A is NOT final delta, so A goes before B
+		return ab
 
 	keys = ['point', 'point1', 'point2']
 	sources = []
@@ -53,15 +54,15 @@ def compare(A, B):
 		A_isAlign       = A['code'] in ['alignh', 'alignv']
 		B_isAlign       = B['code'] in ['alignh', 'alignv']
 		if A_isAlign and B_isMiddleDelta: # special case
-			return (True, False) # order == A->B
-		elif A_isMiddleDelta and B_isAlign: # special
-			return (True, True) # order == B->A
+			return ab
+		elif B_isAlign and A_isMiddleDelta : # symmetrical special case
+			return ba
 		else:
 			print "COMPARE_LOOP",A,B
 			raise Exception("loop")
-	if   AbeforeB: return (True, False)
-	elif BbeforeA: return (True, True)
-	else: return (False, False)
+	if   AbeforeB: return ab
+	elif BbeforeA: return ba
+	else: return dontcare
 
 def sort(cmds):
 	x, ytb, y, fdeltah, fdeltav = [], [], [], [], []
