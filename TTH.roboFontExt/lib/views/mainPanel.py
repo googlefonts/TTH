652ad29ccc46747a0d2b2856619d147f1108764c
Jeremie Hornus <jeremie.hornus@typefactory.net>
2015-06-09 23:00:42 +0200

    … fix

--- TTH.roboFontExt/lib/views/mainPanel.py
+++ TTH.roboFontExt/lib/views/mainPanel.py
@@ -167,8 +167,8 @@ class MainPanel(BaseWindowController):
 
 		self.wTools.gear.setItems(
 			[firstItem,
-			"Auto-Hint…", # 1
-			"Auto-Match…", # 2
+			u"Auto-Hint…", # 1
+			u"Auto-Match…", # 2
 			NSMenuItem.separatorItem(),
 			"Monochrome", # 4
 			"Grayscale", # 5
@@ -178,9 +178,9 @@ class MainPanel(BaseWindowController):
 			"Program", # 9
 			"Assembly", # 10
 			NSMenuItem.separatorItem(),
-			"Control Values…", # 12
+			u"Control Values…", # 12
 			NSMenuItem.separatorItem(),
-			"Preferences…", # 14
+			u"Preferences…", # 14
 			NSMenuItem.separatorItem(),
 			"Compile all TTF data", # 16
 			]
