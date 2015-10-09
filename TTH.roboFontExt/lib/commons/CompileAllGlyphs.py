76ca89a730bd40b2b5e8e7ae93cde5cbc1ab9616
Samuel Hornus <samuel.hornus@inria.fr>
2015-10-09 11:50:58 +0200

    Ship Font and Compile All Glyphs scripts

--- TTH.roboFontExt/lib/commons/CompileAllGlyphs.py
+++ TTH.roboFontExt/lib/commons/CompileAllGlyphs.py
@@ -1,1 +1,19 @@
 print 'Should compile all glyphs assembly'
+
+from robofab.interface.all.dialogs import Message as FabMessage
+
+from models.TTHTool import uniqueInstance as tthTool
+from commons import helperFunctions
+
+def go():
+	font = CurrentFont()
+	if (font is None) or (not helperFunctions.fontIsQuadratic(font)):
+		FabMessage("This is not a Quadratic/TrueType UFO.\nNothing to do.")
+		return
+
+	fm = tthTool.fontModelForFont(font)
+	#fm.compileAllGlyphs()
+	fm.compileAllTTFData()
+	print "TTH data compiled for font", font.fileName
+
+go()
--- TTH.roboFontExt/lib/commons/ShipFont.py
+++ TTH.roboFontExt/lib/commons/ShipFont.py
@@ -1,1 +1,36 @@
-print 'Should compile TTF Font'
+#print 'Should compile TTF Font'
+
+from robofab.interface.all.dialogs import Message as FabMessage
+
+from models.TTHTool import uniqueInstance as tthTool
+from commons import helperFunctions
+
+def go():
+	font = CurrentFont()
+	if (font is None) or (not helperFunctions.fontIsQuadratic(font)):
+		FabMessage("This is not a Quadratic/TrueType UFO.\nNothing to do.")
+		return
+
+	fname = font.fileName
+	if fname[-4:] != ".ufo":
+		FabMessage("This is not a UFO.\nNothing to do.")
+		return
+
+	fname = fname[-4:]+".ttf"
+
+	fm = tthTool.fontModelForFont(font)
+	if fm is None:
+		FabMessage("Shipping Font Error. Sorry. Bye.")
+		return
+
+	fm.compileAllTTFData()
+	font.generate(fname, 'ttf',\
+			decompose     = False,\
+			checkOutlines = False,\
+			autohint      = False,\
+			releaseMode   = False,\
+			glyphOrder    = None,\
+			progressBar   = None)
+	print "TTF font", fname, "shipped."
+
+go()
