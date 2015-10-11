e5741979a0e886bf81572cb6d288598bc7565bb9
Samuel Hornus <samuel.hornus@inria.fr>
2015-10-11 19:33:00 +0200

    short script to hide code

--- TTH.roboFontExt/lib/commons/ClearAllTTHData.py
+++ TTH.roboFontExt/lib/commons/ClearAllTTHData.py
@@ -1,40 +1,4 @@
 
-from robofab.interface.all.dialogs import Message as FabMessage
-from robofab.interface.all.dialogs import AskYesNoCancel as AskYesNoCancel
-
-from models.TTHTool import uniqueInstance as tthTool
-from commons import helperFunctions
-from views import TTHProgressWindow
-reload(TTHProgressWindow)
-
-def go():
-	font = CurrentFont()
-	if (font is None) or (not helperFunctions.fontIsQuadratic(font)):
-		FabMessage("This is not a Quadratic/TrueType UFO.\nNothing to clean.")
-		return
-
-	choice = AskYesNoCancel('Shall I really clear the TTH data in UFO\n\t{} ?'.format(font.fileName),
-			title="TTH",
-			default=0,
-			informativeText="(You might lose a lot of hard work.)")
-	if choice != 1:
-		return
-	fm = tthTool.fontModelForFont(font)
-	if fm is None:
-		FabMessage("Can't find the TTHFont instance. Sorry. Bye.")
-		return
-	pbw = TTHProgressWindow.TTHProgressWindow("Clear all TTH data...", len(font))
-	exn = None
-	try:
-		fm.purgeHintingData(pbw)
-	except Exception as inst:
-		exn = inst
-	finally:
-		if pbw: pbw.close()
-	if exn:
-		print "[TTH ERROR] An error happened during the compilation of the glyphs' hinting program in font", font.fileName
-		print exn
-	else:
-		print "TTH data purged from font", font.fileName
-
-go()
+from commons import DoClearAllTTHData
+reload(DoClearAllTTHData)
+DoClearAllTTHData.go()
--- TTH.roboFontExt/lib/commons/CompileAllGlyphs.py
+++ TTH.roboFontExt/lib/commons/CompileAllGlyphs.py
@@ -1,33 +1,4 @@
 
-from robofab.interface.all.dialogs import Message as FabMessage
-
-from models.TTHTool import uniqueInstance as tthTool
-from commons import helperFunctions
-from views import TTHProgressWindow
-reload(TTHProgressWindow)
-
-def go():
-	font = CurrentFont()
-	if (font is None) or (not helperFunctions.fontIsQuadratic(font)):
-		FabMessage("This is not a Quadratic/TrueType UFO.\nNothing to do.")
-		return
-	exn = None
-	fm = tthTool.fontModelForFont(font)
-	if fm is None:
-		FabMessage("Can't find the TTHFont instance. Sorry. Bye.")
-		return
-	pbw = TTHProgressWindow.TTHProgressWindow("Compiling all glyphs...", len(font))
-	try:
-		if pbw: pbw.setInfo("(please wait)")
-		fm.compileAllGlyphs(pbw)
-	except Exception as inst:
-		exn = inst
-	finally:
-		if pbw: pbw.close()
-	if exn:
-		print "[TTH ERROR] An error happened during the compilation of the glyphs' hinting program in font", font.fileName
-		print exn
-	else:
-		print "TTH data compiled for font", font.fileName
-
-go()
+from commons import DoCompileAllGlyphs
+reload(DoCompileAllGlyphs)
+DoCompileAllGlyphs.go()
