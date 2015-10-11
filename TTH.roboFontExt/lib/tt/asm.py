3b3ed387a0df3e5f27645bad569f118e5b7680ae
Jeremie Hornus <jeremie.hornus@typefactory.net>
2015-10-11 17:51:22 +0200

    bug fix delta ranges

--- TTH.roboFontExt/lib/tt/asm.py
+++ TTH.roboFontExt/lib/tt/asm.py
@@ -431,7 +431,7 @@ def processDeltaCommand(command, pointNameToIndex):
 		deltaInstructions.append(tables.autoPush(*deltaPList))
 		deltaInstructions.append('DELTAP1[ ]')
 
-	elif deltasP2:
+	if deltasP2:
 		for relativeSize in deltasP2:
 			arg = ((relativeSize -16) << 4 ) + tables.stepToSelector[step]
 			deltaPList += [arg, pointIndex]
@@ -439,7 +439,7 @@ def processDeltaCommand(command, pointNameToIndex):
 		deltaInstructions.append(tables.autoPush(*deltaPList))
 		deltaInstructions.append('DELTAP2[ ]')
 
-	elif deltasP3:
+	if deltasP3:
 		for relativeSize in deltasP3:
 			arg = ((relativeSize -32) << 4 ) + tables.stepToSelector[step]
 			deltaPList += [arg, pointIndex]
