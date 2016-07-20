ac6618bac6e0d3eebb172d8bb4237b3b1e02d940
Samuel Hornus <samuel.hornus@inria.fr>
2016-07-20 19:53:07 +0200

    more info messages

--- TTH.roboFontExt/lib/models/TTHFont.py
+++ TTH.roboFontExt/lib/models/TTHFont.py
@@ -583,7 +583,7 @@ When do we regenerate a partial font?
 		os.rename(fontpath_glyph, destination)
 
 	def generatePartialTempFont(self, glyphSet):
-		#try:
+		try:
 			tempFont = RFont(showUI=False)
 			info = self.f.info
 			tempFont.info.unitsPerEm = info.unitsPerEm
@@ -623,14 +623,17 @@ When do we regenerate a partial font?
 					releaseMode   = False,
 					glyphOrder    = None,
 					progressBar   = None )
-		#except:
-		#	print 'ERROR: Unable to generate temporary font'
+		except:
+			print 'ERROR: Unable to generate temporary font. Got the following report:'
+			print self.lastFontGenerationReport
+		print 'SUCCESS: generated temporary font. Got the following report:'
+		print self.lastFontGenerationReport
 
 	def updatePartialFont(self, glyphSet):
 		"""Typically called directly when the current glyph has been modifed."""
-		if HF.fontIsQuadratic(self.f):
-			self.generatePartialTempFont(glyphSet)
-			self.regenTextRenderer()
+		if not HF.fontIsQuadratic(self.f): return
+		self.generatePartialTempFont(glyphSet)
+		self.regenTextRenderer()
 
 	def updatePartialFontIfNeeded(self, g, curSet):
 		"""Re-create the partial font if new glyphs are required."""
